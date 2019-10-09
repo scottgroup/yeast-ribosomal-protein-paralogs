e615a243429c6f6256334f9ac8c3638f7430e9bb
gaspard <gaspard.reulet@usherbrooke.ca>
2019-10-09 14:12:08 -0400

    Changed get_output.bcl2fastq so that it ouputs all the file names for the snakemake rule all

--- scripts/get_output.py
+++ scripts/get_output.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 import os
 
-def bcl2fastq(path):
+def bcl2fastq(wildcards):
     """
     A typical fastq file named by the bcl2fastq software is:
     samplename_samplerank_run[12]_001.fastq.gz
     """
-    sample_file = os.path.join(path, 'SampleSheet.csv')
-    found_data = False
-    line_number = 1
-    samples = []
-    with open(sample_file) as file:
-        for i, line in enumerate(file):
-            if line.startswith('Sample_ID'):
-                found_data = True
-                continue
+    datasets = config['downloads']['datasets_all']
+    path = config['path']['bcl']
+    all_samples = []
+    for dataset in datasets:
+        sample_file = os.path.join(path, dataset, 'SampleSheet.csv')
+        found_data = False
+        line_number = 1
+        samples = []
+        with open(sample_file) as file:
+            for i, line in enumerate(file):
+                if line.startswith('Sample_ID'):
+                    found_data = True
+                    continue
 
-            if found_data:
-                l = line.strip('\n')
-                col = l.split(',')
-                samples.append(f'{col[1]}_S{line_number}_R1_001.fastq.gz')
-                samples.append(f'{col[1]}_S{line_number}_R2_001.fastq.gz')
-                line_number += 1
+                if found_data:
+                    l = line.strip('\n')
+                    col = l.split(',')
+                    samples.append(f'{col[1]}_S{line_number}_R1_001.fastq.gz')
+                    samples.append(f'{col[1]}_S{line_number}_R2_001.fastq.gz')
+                    line_number += 1
+        all_samples.extend(samples)
     # print(samples)
-    return samples
+    return all_samples
 
 
 if __name__ in '__main__':
